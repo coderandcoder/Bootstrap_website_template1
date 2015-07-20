6f33ac1ce1ff30f8b2f0b9bb95291db281f1049b
coderandcoder <coderandcoder@users.noreply.github.com>
2015-07-20 10:52:43 +0530

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,24 @@
+/**
+ * Created by anandraj on 7/14/2015.
+ */
+
+
+$('#app_maint').click(function(){
+    $('#app_maint_modal').modal('show')
+});
+
+$('#data_desi').click(function(){
+    $('#data_desi_modal').modal('show')
+});
+
+$('#data_dev').click(function(){
+    $('#data_dev_modal').modal('show')
+});
+
+$('#data_mig').click(function(){
+    $('#data_mig_modal').modal('show')
+});
+
+$('#data_reen').click(function(){
+    $('#data_reen_modal').modal('show')
+});
