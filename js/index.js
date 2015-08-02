cb96da5ff3a91ab422821e5340adda483620f1b5
coderandcoder <coderandcoder@gmail.com>
2015-08-02 16:18:35 +0530

    index update

--- js/index.js
+++ js/index.js
@@ -26,6 +26,20 @@ $('#data_reen').click(function(){
 
 
 
+<!-- Start of StatCounter Code for Default Guide -->
+
+var sc_project=10555606;
+var sc_invisible=0;
+var sc_security="1b42f14f";
+var scJsHost = (("https:" == document.location.protocol) ?
+    "https://secure." : "http://www.");
+/*document.write("<sc"+"ript type='text/javascript' src='" +
+    scJsHost+
+    "statcounter.com/counter/counter.js'></"+"script>");*/
+
+
+<!-- End of StatCounter Code for Default Guide -->
+
 
 
 
